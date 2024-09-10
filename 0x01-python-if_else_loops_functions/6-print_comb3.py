87d2fa4f647868e288ad63f5a3b0089a743f4e5a
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-10 13:55:04 -0700

    Fix trailing comma in 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 for i in range(10):
     for j in range(i + 1, 10):
-        if i < 9 or j < 9:
-            print("{:02d}, ".format(i * 10 + j), end="")
-        else:
+        if i == 8 and j == 9:
             print("{:02d}".format(i * 10 + j))
+        else:
+            print("{:02d}, ".format(i * 10 + j), end="")
