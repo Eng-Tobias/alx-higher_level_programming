7938f03a1a5f91a77f3b0802f7615575aca52c0b
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-12 16:56:34 -0700

    Correct implementation of 0-add.py task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
+# Import the add function from add_0 module
 from add_0 import add
 
+# Assign values to variables a and b
 a = 1
 b = 2
+
+# Use the print function with string formatting to display the result
 print(f"{a} + {b} = {add(a, b)}")
