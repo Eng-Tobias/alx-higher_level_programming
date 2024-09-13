936f709c13fcd22674a3dbd714633a95347a56b3
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-12 18:51:21 -0700

    Renamed 102-magic_calculation.py to 102_magic_calculation.py and updated imports in test_magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/test_magic_calculation.py
@@ -0,0 +1,7 @@
+# test_magic_calculation.py
+
+from 102_magic_calculation import magic_calculation
+
+print(magic_calculation(2, 3, 4))  # Output should be 4 because 2 < 3
+print(magic_calculation(5, 3, 4))  # Output should be 8 because 5 > 4
+print(magic_calculation(5, 6, 4))  # Output should be 26 because 6 > 4
