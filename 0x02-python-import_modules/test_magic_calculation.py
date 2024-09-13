6c75e54f48c7326478ff86b7cc19db944a9b007b
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-12 18:20:55 -0700

    Add solutions for tasks 6, 7, 8, and 9; including advanced Python scripts

---
+++ 0x02-python-import_modules/test_magic_calculation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from magic_calculation import magic_calculation
+
+print(magic_calculation(1, 2))  # Expected output: 1 + 2 + 4 + 5 = 12
+print(magic_calculation(5, 3))  # Expected output: 5 - 3 = 2
