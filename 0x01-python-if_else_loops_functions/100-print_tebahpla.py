47fd12c7d8809258cf02a7854675308d15b71ba1
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-12 18:39:29 -0700

    Add 100-print_tebahpla.py that prints the alphabet in reverse alternating cases

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-print(f"{''.join(chr(i) if i % 2 else chr(i - 32) for i in range(122, 64, -1))}")
+print("".join(chr(122 - i) if i % 2 == 0 else chr(90 - i) for i in range(26)))
