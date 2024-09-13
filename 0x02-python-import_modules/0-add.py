fdaf165f774a99afa7780f869fdbcce216cfe7c1
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-12 17:20:51 -0700

    Add 0-add.py script to import function and print result

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
 
-def main():
-    a = 1
-    b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
-
-if __name__ == "__main__":
-    main()
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
