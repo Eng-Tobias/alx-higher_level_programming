7bdc2fc00a6659d330a85e7001fb8f392dc09382
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-12 19:07:53 -0700

    Fix magic_calculation function and test script

--- 0x01-python-if_else_loops_functions/test_magic_calculation.py
+++ 0x01-python-if_else_loops_functions/test_magic_calculation.py
@@ -1,7 +1,7 @@
-# test_magic_calculation.py
+#!/usr/bin/python3
 
 from 102_magic_calculation import magic_calculation
 
-print(magic_calculation(2, 3, 4))  # Output should be 4 because 2 < 3
-print(magic_calculation(5, 3, 4))  # Output should be 8 because 5 > 4
-print(magic_calculation(5, 6, 4))  # Output should be 26 because 6 > 4
+print(magic_calculation(2, 3, 4))  # Expected output: 4
+print(magic_calculation(5, 3, 4))  # Expected output: 8
+print(magic_calculation(5, 6, 4))  # Expected output: 26
