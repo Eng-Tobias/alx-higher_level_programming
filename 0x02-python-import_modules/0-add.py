40a2328af139ce8316cb4395dd7985e43d881d6f
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-12 17:08:11 -0700

    Optimize 0-add.py to meet task requirements

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-# Import the add function from add_0 module
 from add_0 import add
 
-# Assign values to variables a and b
 a = 1
 b = 2
-
-# Use the print function with string formatting to display the result
 print("{} + {} = {}".format(a, b, add(a, b)))
