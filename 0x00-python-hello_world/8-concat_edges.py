ec2e2bf667ed435cc3405cfd062bd56b066a1e7b
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-13 19:01:29 -0700

    Update 8-concat_edges.py to print the correct sentence without using string literals

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "object" + "-" + "oriented "
-str2 = "programming" + " with " + "Python"
-print(str1 + str2)
+str1 = "object-oriented" + " " + "programming"
+str2 = "with" + " " + "Python"
+print(f"{str1} {str2}")
