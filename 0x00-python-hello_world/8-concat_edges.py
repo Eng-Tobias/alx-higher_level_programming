ce0b4f60bbba070357feec4c67c095aa372a6fb5
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-13 18:59:05 -0700

    Fix 8-concat_edges.py to print the correct output without extra spaces

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = 'object' + '-' + 'oriented '
-str2 = 'programming' + ' ' + 'with '
-str3 = 'Python'
-print(' '.join([str1, str2, str3]))
+str1 = "object" + "-" + "oriented "
+str2 = "programming" + " with " + "Python"
+print(str1 + str2)
