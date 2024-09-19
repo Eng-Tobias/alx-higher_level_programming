571669037231e983890b6c1d4c96a33b6de2baae
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-18 19:02:51 -0700

    Task 7, 11 and 12

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-Function that replaces or adds key-value in a dictionary.
-"""
-
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
