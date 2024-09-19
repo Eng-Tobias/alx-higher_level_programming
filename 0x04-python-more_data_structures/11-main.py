3b7b14958dfde1814fc8f778f36437c113da310c
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-18 18:38:35 -0700

    Task 11: Implemented function to multiply list elements using map

--- 0x04-python-more_data_structures/11-main.py
+++ 0x04-python-more_data_structures/11-main.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-update_dictionary = __import__('11-multiply_by_2').update_dictionary
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
 
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14}
-print_sorted_dictionary(a_dictionary)
-
-update_dictionary(a_dictionary, 'John', 89)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
-update_dictionary(a_dictionary, 'Sarah', 40)
-print("--")
-print_sorted_dictionary(a_dictionary)
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)
