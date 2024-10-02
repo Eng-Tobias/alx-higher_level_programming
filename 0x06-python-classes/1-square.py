6c38e55d5127d97d268c38bec867d86f3288a5e3
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-01 18:11:32 -0700

    Implemented Square class with size validation, area method, and printing functionality

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a class `Square` with a private attribute `size`.
+"""
+
+class Square:
+    """
+    A class that defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with a private instance attribute `size`.
+        """
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+# 0x06. Python - Classes and Objects
+
+This project covers the basics of Object-Oriented Programming (OOP) in Python. We learn about classes, objects, methods, attributes, and more.
