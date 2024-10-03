9d5bfa48f1b3f0c50ead3f27620b9af863f93cfc
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-02 19:00:31 -0700

    Implement magic_calculation for tasks 9 and 10

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,39 +1,13 @@
 #!/usr/bin/python3
-import sys
 
-def safe_function(fct, *args):
-    try:
-        return fct(*args)  # Call the function with provided arguments
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)  # Print the exception to stderr
-        return None  # Return None if an error occurs
-
-# Example functions to test safe_function
-def print_message():
-    print("This is a message.")
-
-def my_div(a, b):
-    return a / b  # Division function
-
-def print_list(my_list, n):
-    for i in range(n):
-        print(my_list[i])  # Print elements of the list
-
-# Testing safe_function with different cases
-if __name__ == "__main__":
-    # Test case: Calling print_message safely
-    result = safe_function(print_message)
-    
-    # Test case: Correct division
-    result = safe_function(my_div, 10, 2)
-    print("Result of division:", result)
-
-    # Test case: Division by zero
-    result = safe_function(my_div, 10, 0)
-    print("Result of division by zero:", result)
-
-    # Test case: Printing list with valid n
-    result = safe_function(print_list, [1, 2, 3, 4], 2)
-
-    # Test case: Printing list with n greater than the list length
-    result = safe_function(print_list, [1, 2, 3, 4], 10)
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return result
--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,12 +1,52 @@
 #include <Python.h>
 #include <stdio.h>
 
-void print_python_list(PyObject *p)
+void print_python_list(PyObject *p) {
+    if (!PyList_Check(p)) {
+        printf("[ERROR] Invalid List Object\n");
+        return;
+    }
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", allocated);
+    for (Py_ssize_t i = 0; i < size; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        printf("Element %zd: ", i);
+        if (PyBytes_Check(item)) {
+            printf("bytes\n");
+            print_python_bytes(item);
+        } else if (PyFloat_Check(item)) {
+            printf("float\n");
+            print_python_float(item);
+        } else {
+            printf("Unknown\n");
+        }
+    }
 }
 
 void print_python_bytes(PyObject *p) {
+    if (!PyBytes_Check(p)) {
+        printf("[ERROR] Invalid Bytes Object\n");
+        return;
+    }
+    Py_ssize_t size = PyBytes_Size(p);
+    printf("[.] bytes object info\n");
+    printf("  size: %zd\n", size);
+    printf("  trying string: %s\n", PyBytes_AsString(p));
+    printf("  first %zd bytes: ", size < 10 ? size : 10);
+    for (Py_ssize_t i = 0; i < (size < 10 ? size : 10); i++) {
+        printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
+    }
+    printf("\n");
 }
 
 void print_python_float(PyObject *p) {
-
+    if (!PyFloat_Check(p)) {
+        printf("[ERROR] Invalid Float Object\n");
+        return;
+    }
+    double value = PyFloat_AsDouble(p);
+    printf("[.] float object info\n");
+    printf("  value: %f\n", value);
 }
