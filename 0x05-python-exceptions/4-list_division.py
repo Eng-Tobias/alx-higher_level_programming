1c5c26df729c5b65bc7da298e089cad1163583af
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-02 18:12:35 -0700

    Fix task 4: Implement error handling in list_division function

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,14 +4,18 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            result.append(my_list_1[i] / my_list_2[i])
+            # Attempt to divide the elements
+            division_result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
+            division_result = 0  # Default value for division by zero
         except TypeError:
             print("wrong type")
-            result.append(0)
+            division_result = 0  # Default value for wrong type
         except IndexError:
             print("out of range")
-            result.append(0)
+            division_result = 0  # Default value for out of range
+        finally:
+            # Append the result (whether it was successful or a default value)
+            result.append(division_result)
     return result
