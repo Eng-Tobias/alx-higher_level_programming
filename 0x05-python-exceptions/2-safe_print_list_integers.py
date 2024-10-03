304a0b4d1c5e00d888bcdd9c1591484fb05a376a
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-02 18:01:04 -0700

    Add executable scripts for Task 2 and Task 4

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-        except IndexError:
-            break
-    print()
-    return count
+def main():
+    mylist = [1, 2, 3, 4]
+    x = len(mylist) + 4
+    print(x)
+
+if __name__ == "__main__":
+    main()
