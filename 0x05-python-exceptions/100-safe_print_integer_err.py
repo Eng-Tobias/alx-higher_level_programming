bbfc8143c72974cd7a885662bb35baf50c57f020
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-02 18:48:23 -0700

    Corrections

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 
 def safe_print_integer_err(value):
@@ -7,3 +8,9 @@ def safe_print_integer_err(value):
     except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
+
+# Test cases
+if __name__ == "__main__":
+    values = [89, -89, "89", '89', 89.9, 0, None, [89]]
+    for v in values:
+        has_been_print = safe_print_integer_err(v)
