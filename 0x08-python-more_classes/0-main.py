a654482e071ff3558e37d1640d1222a0f79e1dc0
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-09 17:26:13 -0700

    Task o to 3

---
+++ 0x08-python-more_classes/0-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-main.py
+
+from module_name import ClassName
+
+# Create an instance of the class
+instance = ClassName()
+
+# Perform operations or call methods on the instance
+print(instance)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# 0x08 Python - More Classes and Objects
+This project contains tasks that implement and define different functionalities of a `Rectangle` class in Python.
