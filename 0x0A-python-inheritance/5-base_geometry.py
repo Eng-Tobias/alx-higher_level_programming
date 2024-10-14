d2d6a72942e1b03955892d7be4da60c97c316825
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-14 12:25:40 -0700

    Add BaseGeometry class and test

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class that serves as a base for geometric shapes.
+    """
+    pass
