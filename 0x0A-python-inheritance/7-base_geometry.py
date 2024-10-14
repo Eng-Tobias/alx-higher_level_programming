5730108b36b77c3e1eec1f5cbeae9b20a73e6bd1
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-14 12:43:11 -0700

    Add test documentation for BaseGeometry class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A class that represents basic geometric properties.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
