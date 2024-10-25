b39c63be90830769f379e5b9a8a1e5c183485b2b
Eng-Tobias <engtobiasguya@gmail.com>
2024-10-25 13:33:34 -0700

    Added update method to Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,11 +37,9 @@ class Rectangle(Base):
         """
         Print the rectangle with the character # using the x and y attributes.
         """
-        # Print y spaces first
         for _ in range(self.y):
             print()
 
-        # Print rectangle
         for _ in range(self.height):
             print(' ' * self.x + '#' * self.width)
 
@@ -52,3 +50,26 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height
         )
+
+    def update(self, *args):
+        """
+        Update the Rectangle attributes using the provided arguments.
+
+        Args:
+            *args: Variable length argument list.
+                1st argument is id
+                2nd argument is width
+                3rd argument is height
+                4th argument is x
+                5th argument is y
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
