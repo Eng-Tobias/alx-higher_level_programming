104a347c1208293643e38bc1bd2a6b719f0a6b6d
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-22 02:18:02 -0800

    Add script to list number of records by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- This script lists the number of records with the same score in the second_table.
+-- The results are sorted by the number of records (in descending order).
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
