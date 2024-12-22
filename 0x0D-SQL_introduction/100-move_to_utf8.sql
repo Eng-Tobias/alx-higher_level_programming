31380f94c4e565fe3e0bab2f1fd7e6aa81b49b2e
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-22 02:30:57 -0800

    Fix: remove explicit CHARACTER SET in column definition

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,5 +7,5 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- Convert the table `first_table` to UTF8 (utf8mb4 character set and utf8mb4_unicode_ci collation)
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the `name` column in the `first_table` to UTF8 (utf8mb4 character set and utf8mb4_unicode_ci collation)
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Modify the `name` column without explicitly specifying `CHARACTER SET utf8mb4`
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
