889a27559afa59d9a0c4a982c5538c28c9296adb
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-22 02:12:56 -0800

    Add script to update Bob's score to 10 in second_table

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,6 @@
- --Updates the score of Bob to 10 in the table second_table
+-- This script updates the score of Bob to 10 in the second_table.
+-- It uses the name field and not the id to find the record.
+
 UPDATE second_table
 SET score = 10
 WHERE name = 'Bob';
