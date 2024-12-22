d0bd280c48a37716ada964b2f481084f338c1d2f
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-22 03:01:38 -0800

    Corrected the character set and collation for first_table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,10 @@
--- Select the database
+-- Convert the database, table, and column to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 USE hbtn_0c_0;
 
--- Convert the database `hbtn_0c_0` to UTF8 (utf8mb4 character set and utf8mb4_unicode_ci collation)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- Convert the table `first_table` to UTF8 (utf8mb4 character set and utf8mb4_unicode_ci collation)
+-- Modify the first_table to use utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Modify the `name` column without explicitly specifying `CHARACTER SET utf8mb4`
+-- Modify the 'name' column in first_table to use utf8mb4_unicode_ci collation
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
