e845e6679cf3b056fd906d87a11566b5911684ff
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-22 01:43:51 -0800

    Create 0-list_databases.sql script to list databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,10 +1,1 @@
--- List initial databases
-SHOW DATABASES;
-
--- Create new databases, if they don't already exist
-CREATE DATABASE IF NOT EXISTS my_db_01;
-CREATE DATABASE IF NOT EXISTS my_db_02;
-CREATE DATABASE IF NOT EXISTS my_db_03;
-
--- List databases after creation
 SHOW DATABASES;
