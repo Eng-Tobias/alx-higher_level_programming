58f40f62521add1d2a7cb02dd9eee4b3f3f67062
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-22 01:57:47 -0800

    Added script to print full table description for first_table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- This script prints the full description of the first_table in the specified database
+-- It uses SHOW CREATE TABLE to get the complete table definition
+
+SHOW CREATE TABLE first_table;
