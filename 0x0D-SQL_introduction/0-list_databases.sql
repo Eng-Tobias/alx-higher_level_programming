2ded641750893975afbc8e128ffbc6cb01cdd2be
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-22 01:35:50 -0800

    Updated SQL scripts for correct output format

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,15 @@
--- List all databases in the MySQL server
-SELECT SCHEMA_NAME
-FROM INFORMATION_SCHEMA.SCHEMATA;
+-- List initial databases
+SELECT schema_name AS 'Database'
+FROM information_schema.schemata
+WHERE schema_name NOT IN ('information_schema', 'performance_schema', 'mysql', 'sys');
+
+-- Create multiple databases
+CREATE DATABASE my_db_01;
+CREATE DATABASE my_db_02;
+CREATE DATABASE my_db_03;
+CREATE DATABASE holbteron_db;
+
+-- List databases again after creation
+SELECT schema_name AS 'Database'
+FROM information_schema.schemata
+WHERE schema_name NOT IN ('information_schema', 'performance_schema', 'mysql', 'sys');
